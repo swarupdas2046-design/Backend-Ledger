f0fef870efabaa8ea31a7d79316afc6c0923ee46
Swarup Das <swarupdas2046@gmail.com>
2026-08-17 14:15:43 +0530

    feat: implement database connection setup in database.js and integrate it in server.js

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 import "dotenv/config"
 import app from "./src/app.js"
+import connectDB from "./src/config/database.js"
 
 const port = Number(process.env.PORT) || 5000
+
+connectDB()
 
 app.listen(port, () => {
     console.log(`Server running on port`, port)
 })
+
 
 
 
