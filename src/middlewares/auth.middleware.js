18b6681a770df6c479f1dc1899dd45eb1ded440c
Swarup Das <swarupdas2046@gmail.com>
2026-08-18 16:37:56 +0530

    feat: add authentication middleware for token verification and user validation

---
+++ src/middlewares/auth.middleware.js
@@ -0,0 +1,25 @@
+import authModel from "../models/auth.model.js";
+import ApiError from "../utils/apiError.js";
+import { Verify_AccessToken } from "../utils/token.js";
+
+const authMiddleware = async (req, res, next) => {
+  try {
+    const AccessToken = req.cookies.AccessToken;
+    if (!AccessToken) throw new ApiError(401, "Token not found");
+
+    const decode = Verify_AccessToken(AccessToken);
+
+    if (!decode) throw new ApiError(401, "invalid token");
+
+    const isExistedUser = await authModel.findById(decode.id).select("-password -refreshToken");
+
+    if (!isExistedUser) throw new ApiError(401, "Unauthorized Access");
+
+    req.user = isExistedUser;
+    next();
+  } catch (error) {
+    next(error);
+  }
+};
+
+export default authMiddleware;
