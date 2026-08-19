2e4bf37a614799fb3a8b8307b99fe371b9004315
Swarup Das <swarupdas2046@gmail.com>
2026-08-20 00:12:37 +0530

    feat: add account model with user association, status, and currency fields

---
+++ src/models/account.model.js
@@ -0,0 +1,30 @@
+import mongoose from "mongoose";
+
+const accountSchema = new mongoose.Schema({
+    user:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"UserAuth",
+        required:[true,"account must have associated with a user"],
+        index:true
+    },
+    status:{
+        type:String,
+        enum:{
+            values:["Active","Frozen","Closed"],
+            message:"Status must be Active, Frozen or Closed"
+        }
+    },
+    currency:{
+        type:String,
+        required:[true,"currency required for creation of account"],
+        default:"INR"
+    },
+},{timestamps:true})
+
+accountSchema.index({user:1,status:1})
+
+
+
+const accountModel = mongoose.model("Account",accountSchema)
+
+export default accountModel
