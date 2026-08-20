7c23552aa6633b628350ee80a3f1693492f4f732
Swarup Das <swarupdas2046@gmail.com>
2026-08-20 23:05:07 +0530

    feat: add transaction notification emails for successful and failed transactions

--- src/utils/emailTemplate.js
+++ src/utils/emailTemplate.js
@@ -136,4 +136,44 @@ Concurrent Bank`
     await sendEmail(email,subject,text,html)
 }
 
+export const SendTransactionEmail = async(email,name,amount,toAccount )=>{
+  const subject = "Transaction Notification"
+  const text = `You have received ${amount} from ${name} to ${toAccount}`
+  const html = `<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  <h1>Transaction Notification</h1>
+  <p>You have received ${amount} from ${name} to ${toAccount}</p>
+</body>
+</html>`
+
+  await sendEmail(email,subject,text,html)
+}
+
+export const SendTransactionFailedEmail = async(email,name,amount,toAccount )=>{
+  const subject = "Transaction Notification"
+  const text = `You have received ${amount} from ${name} to ${toAccount}`
+  const html = `<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  <h1>Transaction Notification</h1>
+  <p>hello ${name}, we regret to inform you that your transaction of ${amount} to ${toAccount} has failed</p>
+</body>
+</html>`
+
+  await sendEmail(email,subject,text,html)
+}
+
 export default emailTemplate
