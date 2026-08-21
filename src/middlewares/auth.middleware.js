0d0d29242d01c24207182ca46c2ae77bafb0595d
Swarup Das <swarupdas2046@gmail.com>
2026-08-22 02:24:39 +0530

    feat: add authSystemMiddleware for system user access control

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -22,4 +22,26 @@ const authMiddleware = async (req, res, next) => {
   }
 };
 
+export const authSystemMiddleware = async (req, res, next) => {
+  try {
+
+    const AccessToken = req.cookies.AccessToken;
+    if (!AccessToken) throw new ApiError(401, "Token not found");
+
+    const decode = Verify_AccessToken(AccessToken);
+
+    if (!decode) throw new ApiError(401, "invalid token");
+
+    const isExistedUser = await authModel.findById(decode.id).select("+systemUser");
+
+    if (!isExistedUser.systemUser) throw new ApiError(403, "Forbidden Access, you are not a system user");
+
+    req.user = isExistedUser;
+    next();
+    
+  } catch (error) {
+    next(error);
+  }
+}
+
 export default authMiddleware;
