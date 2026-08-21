3ce056c2ad3ab6fd351c6301885fe9c7f39a3f2b
Swarup Das <swarupdas2046@gmail.com>
2026-08-22 02:24:53 +0530

    feat: implement getBalance method to calculate account balance based on ledger entries

--- src/models/account.model.js
+++ src/models/account.model.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import ledgerModel from "./ledger.model.js";
 
 const accountSchema = new mongoose.Schema({
     user:{
@@ -23,6 +24,49 @@ const accountSchema = new mongoose.Schema({
 },{timestamps:true})
 
 accountSchema.index({user:1,status:1})
+
+accountSchema.methods.getBalance = async function(){
+    const balanceData = await ledgerModel.aggregate([
+        {$match:{account:this._id}},
+        {
+        
+        $group:{
+                _id:null,
+                totalDebit:{
+                    $sum:{
+                        $cond:[
+                            {$eq:["$type","DEBIT"]},
+                            "$amount",
+                            0
+                        ]
+                    }
+                },
+                totalCredit:{
+                    $sum:{
+                        $cond:[
+                            {$eq:["$type","CREDIT"]},
+                            "$amount",
+                            0
+                        ]
+                    }
+                }
+        }
+        },
+        {
+            $project:{
+                _id:0,
+                balance:{$subtract:["$totalCredit","$totalDebit"]}
+            }
+        }
+
+        
+    ])
+
+    if(balanceData.length === 0){
+        return 0
+    }
+    return balanceData[0].balance
+}
 
 
 // Create and export the UserAuth model based on the authSchema
