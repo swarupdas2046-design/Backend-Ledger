d03d4436dcece2b5b00cb3f867f079171aa16378
Swarup Das <swarupdas2046@gmail.com>
2026-08-22 02:25:10 +0530

    feat: update transaction routes to include createTransaction and initial funds transaction for system users

--- src/routes/transaction.route.js
+++ src/routes/transaction.route.js
@@ -1,10 +1,19 @@
 import express from 'express'
-import authMiddleware from '../middlewares/auth.middleware.js'
+import authMiddleware, { authSystemMiddleware } from '../middlewares/auth.middleware.js'
+import { createInitialFundsTransaction, createTransaction } from '../controllers/transaction.controller.js'
 
 
 const transactionRouter = express.Router()
 
 
-transactionRouter.post("/",authMiddleware)
+transactionRouter.post("/",authMiddleware,createTransaction)
+
+
+/**
+ * - POST /api/transactions/system/initial-funds
+ * - create initial funds transaction foe system user
+ */
+
+transactionRouter.post("/system/initial-funds",authSystemMiddleware,createInitialFundsTransaction)
 
 export default transactionRouter
