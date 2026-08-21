15ede492c8f0d151a0f2e17b39cf6dc4f4a8966f
Swarup Das <swarupdas2046@gmail.com>
2026-08-22 02:25:01 +0530

    feat: add GetUserAccounts and GetAccountBalance routes to account router

--- src/routes/account.route.js
+++ src/routes/account.route.js
@@ -1,17 +1,31 @@
 import express from 'express'
 import authMiddleware from '../middlewares/auth.middleware.js'
-import { CreateAccount } from '../controllers/acounts.controller.js'
+import { CreateAccount, GetAccountBalance, GetUserAccounts } from '../controllers/accounts.controller.js'
 
 const accountRouter = express.Router()
 
 // this is basically a protected routes check authMiddleware
-accountRouter.get("/ALL-Accounts",authMiddleware,(req,res)=>{
-return res.status(200).json({
-        message:"Account fetched SuccessFully"
-    })
-})
+// accountRouter.get("/ALL-Accounts",authMiddleware,(req,res)=>{
+// return res.status(200).json({
+//         message:"Account fetched SuccessFully"
+//     })
+// })
 
 accountRouter.post("/",authMiddleware,CreateAccount)
+
+/**
+ * - GET /api/accounts (GET)
+ * - Get all accounts OF the login user
+ * - Protected route
+ */
+
+accountRouter.get("/",authMiddleware,GetUserAccounts)
+
+/**
+ * - GET /api/accounts/balance/:accountId (GET)
+ */
+
+accountRouter.get("/balance/:accountId",authMiddleware,GetAccountBalance)
 
 export default accountRouter
 
