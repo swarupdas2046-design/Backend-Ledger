41267cba944e7ba0d9ac321fca4765c371027830
Swarup Das <swarupdas2046@gmail.com>
2026-08-22 02:24:14 +0530

    feat: refactor transaction creation logic and enhance initial funds transaction handling

--- src/controllers/transaction.controller.js
+++ src/controllers/transaction.controller.js
@@ -1,30 +1,69 @@
-import ledgerModel from "../models/ledger.model.js";
+import mongoose from 'mongoose'
+import accountModel from "../models/account.model.js";
 import transactionModel from "../models/transaction.model.js";
 import { createService } from "../services/transaction.service.js";
 import ApiError from "../utils/apiError.js";
+import ApiResponse from "../utils/apiResponse.js";
 import asyncHandler from "../utils/asyncHandler.js";
-import { SendTransactionEmail, SendTransactionFailedEmail } from "../utils/emailTemplate.js";
+import ledgerModel from '../models/ledger.model.js';
 
-const createTransaction = asyncHandler(async (req, res) => {
+export const createTransaction = asyncHandler(async (req, res) => {
     // const { fromAccount, toAccount, amount, idempotencyKey } = req.body;
-    const {isTransactionExists} = await createService(req.body)
+    const {statusCode, message,data} = await createService(req.body,req.user)
 
-    /**
-     * - checking if transaction already exists
-     */
 
-    if (isTransactionExists) {
-        if (isTransactionExists.status === "COMPLETED") {
-            return res.status(201).json(new ApiResponse("Transaction was completed",isTransactionExists));
-        }
-        if(isTransactionExists.status === "FAILED") throw new ApiError(500, "Transaction was failed please try again");
-        
-        if(isTransactionExists.status === "PENDING") {
-            return res.status(200).json(new ApiResponse("Transaction Still Processing"));
-        }
-        
-        if(isTransactionExists.status === "REVERSED") throw new ApiError(500, "Transaction was reversed please try again");
-    }
+    return res.status(statusCode).json(new ApiResponse(message,data));
+
+})
+
+
+export const createInitialFundsTransaction = asyncHandler(async (req, res) => {
+    const {toAccount, amount, idempotencyKey} = req.body;
+    if (!toAccount || !amount || !idempotencyKey) throw new ApiError(400, "Please fill all the details");
+
+    const toUserAccount = await accountModel.findOne({ _id: toAccount });
+
+    if (!toUserAccount) throw new ApiError(400, "Invalid toAccount");
+
+    const fromUserAccount = await accountModel.findOne({ 
+        user: req.user._id,
+        // systemUser: true
+    });
+
+    if (!fromUserAccount) throw new ApiError(400, "Invalid system user account");
+
+    const session = await mongoose.startSession();
+    session.startTransaction();
+
+    const transaction = new transactionModel({
+        fromAccount: fromUserAccount._id,
+        toAccount: toUserAccount._id,
+        amount,
+        idempotencyKey,
+        status: "PENDING"
+    },);
+
+    const debitLedgerEntry = await ledgerModel.create([{
+        account: fromUserAccount._id, 
+        amount,
+        type: "DEBIT",
+        transaction: transaction._id,
+    }], {session});
+
+    const creditLedgerEntry = await ledgerModel.create([{
+        account: toUserAccount._id,
+        amount,
+        type: "CREDIT",
+        transaction: transaction._id,
+    }], {session});
+
+    transaction.status = "COMPLETED";
+    await transaction.save({session});
+
+    await session.commitTransaction();
+    session.endSession();
+
+    return res.status(200).json(new ApiResponse("initial funds Transaction was completed",transaction));
 
 
 })
